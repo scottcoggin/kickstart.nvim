4ebea03f6a873459c18943a74e8328af6beea1d5
Scott Coggin <scott.coggin@gmail.com>
2025-04-14 11:54:14 +0000

    os.lua

---
+++ lua/custom/os.lua
@@ -0,0 +1,8 @@
+local os_name = vim.uv.os_uname().sysname
+local M = {}
+
+M.is_windows = os_name:find 'Windows' ~= nil
+M.is_linux = os_name:find 'Linux' ~= nil
+M.is_mac = os_name:find 'Darwin' ~= nil
+
+return M
